f60253dee05efbc4e57a67415c0fb7f2a262b561
GraceTao <graceytao2003@gmail.com>
2023-08-03 11:57:28 -0400

    added the rest of the endpoints

--- server.js
+++ server.js
@@ -8,9 +8,80 @@ const path = require('path');
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
+app.listen(port, () => console.log(`Server has started on port: ${port}`))
 
+app.post('/addstudent', async (req, res) => {
+  const {name, day, st} = req.body
+  const peopleRef = db.collection('attendance').doc(day)
+  const res2 = await peopleRef.set({
+      [name]: {
+          "status": st
+      }
+  }, { merge: true })
+   
+  res.status(200).send("successfully added a new student")
+})
 
+app.get('/getroster', async (req, res) => {
 
+  const { date, status } = req.query;
+  const dateRef = db.collection("attendance").doc(date);
+  const doc = await dateRef.get();
+  const students = [];
+
+  if (!doc.exists) {
+      return res.sendStatus(400)
+  }
+
+  const data = doc.data();
+
+  for (let k in data) {
+   if (data[k]["status"] == status) {
+       students.push(k);
+   }
+  }
+
+  res.status(200).send(students)
+})
+
+app.get('/data', (req, res) => {
+  db.collection("attendance")
+    .get()
+    .then(function(querySnapshot) {
+      var data = [];
+      querySnapshot.forEach(function(doc) {
+        data.push([doc.id,doc.data()]);
+        
+      });
+      console.log(data);
+      res.send(data);
+    })
+    .catch(function(error) {
+      console.log('Error getting Firestore data: ', error);
+      res.status(500).send('Error retrieving Firestore data');
+    });
+});
+
+app.get('/getstudent', async (req, res) => {
+    
+  const name = req.query.name
+  var dict = {};
+  const datesRef = db.collection('attendance');
+const snapshot = await datesRef.orderBy(name).get();
+if (snapshot.empty) {
+  res.status(200).send("We could not find " + name + " in our database")
+  return;
+}
+
+snapshot.forEach(doc => {
+var date = doc.id 
+const data = doc.data()
+dict[date] = data[name]["status"]
+});
+
+res.status(200).send(dict)
+  
+});
 
   app.get('/roster', (req, res) => {
     res.sendFile(path.join(__dirname, 'roster.html'));
@@ -35,4 +106,3 @@ app.use(express.urlencoded({ extended: true }));
 
 
 
-app.listen(port, () => console.log(`Server has started on port: ${port}`))
